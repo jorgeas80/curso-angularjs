21106318b3e3eba0c3d5be4b52ee378556f128b2
jorge <jorge@imedinas-MacBook-Air.local>
2016-03-12 17:54:30 +0100

    Added comment

--- ej05/js/ej05.js
+++ ej05/js/ej05.js
@@ -5,9 +5,13 @@
     // Esta funcion es el controlador que asociamos a la vista del ej05
     function ej05Controller($scope) {
 
+        // Estas variables serán copiadas por valor si alguna directiva dentro de este controlador crea su propio scope heredado (ej: ng-if, ng-switch)
+        // Es decir: EL SCOPE HIJO Y EL PADRE QUEDARÁN DESCONECTADOS EN LO REFERENTE A ESTAS VARIABLES
         $scope.show = true;
         $scope.theField = 'hello';   
         
+        // Estas variables serán copiadas por referencia si alguna directiva dentro de este controlador crea su propio scope heredado (ej: ng-if, ng-switch)
+        // Es decir: EL SCOPE HIJO Y EL PADRE SEGUIRÁN CONECTADOS EN LO REFERENTE A ESTAS VARIABLES
         $scope.obj = {
             show: true,
             theField: 'hello'
