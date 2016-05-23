09dd116b6d622c3950b0c91feaffc498a6a12ddb
Jorge Arevalo <jorgeas80@gmail.com>
2016-05-23 20:13:38 +0200

    Style fix in ej05

--- ej05/js/ej05_pubsub.js
+++ ej05/js/ej05_pubsub.js
@@ -15,8 +15,16 @@
         
     }
 
-    function SiblingOneCtrl() {
+    function SiblingOneCtrl($scope) {
+        var vm = this;
 
+        // Para guardar mensajes
+        vm.messages = [];
+
+        // Escuchamos lo que emite nuestro nieto
+        $scope.$on('ChildCtrl:scope:emit', function(event, data) {
+            vm.messages.push(data);
+        });
     }
 
     function SiblingTwoCtrl($scope) {
@@ -26,7 +34,6 @@
         // Para guardar mensajes
         vm.messages = [];
 
-
         // Escuchamos lo que emite nuestro hijo
         $scope.$on('ChildCtrl:scope:emit', function(event, data) {
            vm.messages.push(data);
@@ -35,8 +42,16 @@
 
     }
 
-    function ParentCtrl() {
+    function ParentCtrl($scope) {
+        var vm = this;
 
+        // Para guardar mensajes
+        vm.messages = [];
+
+        // Como ChildCtrl no es descendiente directo nuestro y emite a través de $scope, NO lo escucharemos a pesar de estar suscritos
+        $scope.$on('ChildCtrl:scope:emit', function(event, data) {
+            vm.messages.push(data);
+        });
     }
 
        
@@ -49,6 +64,8 @@
         .controller('ChildCtrl', ChildCtrl);
 
     ChildCtrl.$inject = ['$scope'];
+    ParentCtrl.$inject = ['$scope'];
+    SiblingOneCtrl.$inject = ['$scope'];
     SiblingTwoCtrl.$inject = ['$scope'];
 
 })();
