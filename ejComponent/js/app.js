120283ecdceea886d9deb61efc5bb1c296b16dae
Jorge Arevalo <jorgeas80@gmail.com>
2016-05-26 18:53:04 +0200

    Added IIFE to directive communication exercise

--- ejComponent/js/app.js
+++ ejComponent/js/app.js
@@ -1,57 +1,59 @@
+(function() {
 
-angular
-    .module('app', [])
-    .directive('tab', tab)
-    .directive('tabs', tabs);
+    angular
+        .module('app', [])
+        .directive('tab', tab)
+        .directive('tabs', tabs);
 
-function tab() {
+    function tab() {
 
-    var f = {};
+        var f = {};
 
-    f.restrict= 'E';
-    f.scope= {
-        label: '@'
-    };
-    f.require= '^tabs';
-    f.transclude= true;
-    f.templateUrl= "partials/tab.html";
-    f.link= function ($scope, $element, $attrs, $ctrl) {
-        $scope.tab = {
-            label: $scope.label,
-            selected: false
+        f.restrict= 'E';
+        f.scope= {
+            label: '@'
         };
-        $ctrl.addTab($scope.tab);
-    };
-
-    return f;
-}
-
-function tabs() {
-
-    var f = {};
-
-    f.restrict = 'E';
-    f.scope = {};
-    f.transclude = true;
-    f.controller = function () {
-        this.tabs = [];
-        this.addTab = function addTab(tab) {
-            this.tabs.push(tab);
+        f.require= '^tabs';
+        f.transclude= true;
+        f.templateUrl= "partials/tab.html";
+        f.link= function ($scope, $element, $attrs, $ctrl) {
+            $scope.tab = {
+                label: $scope.label,
+                selected: false
+            };
+            $ctrl.addTab($scope.tab);
         };
-        this.selectTab = function selectTab(index) {
-            for (var i = 0; i < this.tabs.length; i++) {
-                this.tabs[i].selected = false;
-            }
-            this.tabs[index].selected = true;
+
+        return f;
+    }
+
+    function tabs() {
+
+        var f = {};
+
+        f.restrict = 'E';
+        f.scope = {};
+        f.transclude = true;
+        f.controller = function () {
+            this.tabs = [];
+            this.addTab = function addTab(tab) {
+                this.tabs.push(tab);
+            };
+            this.selectTab = function selectTab(index) {
+                for (var i = 0; i < this.tabs.length; i++) {
+                    this.tabs[i].selected = false;
+                }
+                this.tabs[index].selected = true;
+            };
         };
-    };
-    f.controllerAs = 'tabs';
-    f.link = function ($scope, $element, $attrs, $ctrl) {
-        $ctrl.selectTab($attrs.active || 0);
-    };
-    f.templateUrl = "partials/tabs.html";
+        f.controllerAs = 'tabs';
+        f.link = function ($scope, $element, $attrs, $ctrl) {
+            $ctrl.selectTab($attrs.active || 0);
+        };
+        f.templateUrl = "partials/tabs.html";
 
-    return f;
+        return f;
 
-}
-
+    }
+    
+})();
